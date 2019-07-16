8938f14508b8e5698f0bd75250dca7c5ca69077a
HickDead <HickDead@xs4all.nl>
2019-07-16 20:45:44 +0200

    Big cleanup, version 0.2

--- SerialTracker.ino
+++ SerialTracker.ino
@@ -1,6 +1,6 @@
 
 /*
- *  SerialTracker v0.1
+ *  SerialTracker v0.2
  *  
  *  Copyright (c) 2019 HickDead (https://github.com/HickDead)
  *  
@@ -20,7 +20,9 @@
 
 
 #define SENSOR_MPU6050
-//#define SENSOR_MPU9150   // untested
+//#define SENSOR_MPU9150
+
+// set to 1 if AD0 on the sensor is connected to VCC
 #define MPU_AD0 0
 
 
@@ -31,8 +33,6 @@
  *  https://github.com/kkpoon/CalibrateMPU6050
  *  ==================================
  */
-
-
 #define ACCEL_OFFSET_X     0
 #define ACCEL_OFFSET_Y     0
 #define ACCEL_OFFSET_Z     0
@@ -43,8 +43,9 @@
 // after filling out the numbers above, uncomment this
 //#define CALIBRATE
 
+
 // uncomment these for more periodic output on the serial console
-#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_QUATERNION
 //#define OUTPUT_READABLE_EULER
 //#define OUTPUT_READABLE_YAWPITCHROLL
 //#define OUTPUT_READABLE_REALACCEL
@@ -72,13 +73,14 @@ BluetoothSerial SerialBT;
 
 # define CONS_BAUD_RATE "SerialTracker"
 # define SerialData Serial
-# define SerialCons SerialBT
+// non-data on seperate serial?
+//# define SerialCons SerialBT
 
 // I2C pins
-# define INT_PIN GPIO_NUM_17
-# define SDA_PIN GPIO_NUM_18
-# define SCL_PIN GPIO_NUM_19
-# define VCC_PIN GPIO_NUM_5
+# define INT_PIN GPIO_NUM_17  //
+# define SDA_PIN GPIO_NUM_18  //
+# define SCL_PIN GPIO_NUM_19  //
+# define VCC_PIN GPIO_NUM_5   // arbitrarely chosen, any digital output pin should work
 
 // workaround for bug in MPU6050_6Axis_MotionApps20.h
 # define __PGMSPACE_H_ 1
@@ -89,27 +91,40 @@ BluetoothSerial SerialBT;
  */
 
 # define SerialData SerialUSB
-# define SerialCons Serial1    // TX 18, RX 19 ?
+// non-data on seperate serial?
+//# define SerialCons Serial1   // TX 18, RX 19 ?
 
 // I2C pins
-# define INT_PIN 2       // arbitrarely chosen, any digital pin should work
-///# define SDA_PIN 20    // for reference, not used, fixed?
-///# define SCL_PIN 21    // for reference, not used, fixed?
+# define INT_PIN 2             // arbitrarely chosen, any digital pin should work
+# define SDA_PIN 20            // for reference, not used, fixed?
+# define SCL_PIN 21            // for reference, not used, fixed?
+# define VCC_PIN 5             // arbitrarely chosen, any digital output pin should work
 
-#elif defined( ARDUINO_BLUEPILL_F103C6 ) || defined( ARDUINO_BLUEPILL_F103C8 ) || defined( ARDUINO_BLACKPILL_F103C8 ) || defined( ARDUINO_BLACKPILL_F303CC )
+# define LED_PIN 13
+# define LED_ON HIGH
+# define LED_OFF LOW
+
+#elif defined( ARDUINO_ARCH_STM32 )
 /*
- *  Black-/Blue-pill specific code (untested)
+ *  Black-/Blue-pill specific code
  */
 
 # define SerialData Serial
-# define SerialCons Serial1    // TX PB6, RX PB7 ?
+// non-data on seperate serial?
+//# define SerialCons Serial1     // TX PA9, RX PA10
 
 // I2C pins
-# define INT_PIN PA1
-# define SCL_PIN PB8    // default, black = PB6 ?? (or PB10)
-# define SDA_PIN PB9    // default, black = PB7 ?? (or PB11)
+# define INT_PIN PA1            //
+# define SCL_PIN PB6            // for reference, unused, default
+# define SDA_PIN PB7            // for reference, unused, default
+# define VCC_PIN PB5            // arbitrarely chosen, any output pin should work
 
-#else // ESP32 || ARDUINO_SAM_DUE || ARDUINO_BLACKPILL_F103C8 || ARDUINO_BLUEPILL_F103C6
+#define LED_PIN PC13
+#define LED_ON LOW
+#define LED_OFF HIGH
+
+
+#else // ESP32 || ARDUINO_SAM_DUE || ARDUINO_ARCH_STM32
 /*
  *  Everything else (untested)
  */
@@ -120,67 +135,64 @@ SoftwareSerial SerialSW( 10, 11);
 
 # define CONS_BAUD_RATE  19200
 # define SerialData Serial
-# define SerialCons SerialSW
+// non-data on seperate serial?
+//# define SerialCons SerialSW
 
 // I2C pins
-# define INT_PIN 2
-///#  define SDA_PIN A4   // for reference, not used, fixed
-///#  define SCL_PIN A5   // for reference, not used, fixed
-# define VCC_PIN 5
+# define INT_PIN 2              // or 3
+# define SDA_PIN A4             // for reference, unused, =fixed
+# define SCL_PIN A5             // for reference, unused, =fixed
+# define VCC_PIN 5              // arbitrarely chosen
 
-#endif // ESP32 || ARDUINO_SAM_DUE || ARDUINO_BLUEPILL_F103C6 || ARDUINO_BLUEPILL_F103C8 || ARDUINO_BLACKPILL_F103C8 || ARDUINO_BLACKPILL_F303CC
+#define LED_PIN LED_BUILTIN
+#define LED_ON HIGH
+#define LED_OFF LOW
+
+#endif // ESP32 || ARDUINO_SAM_DUE || ARDUINO_ARCH_STM32
 
 
 #ifndef DATA_BAUD_RATE
 # define DATA_BAUD_RATE 115200
 #endif // DATA_BAUD_RATE
-#ifndef CONS_BAUD_RATE
+
+#ifndef SerialCons
+# undef CONS_BAUD_RATE
+# define SerialCons SerialData
+#elif ! defined( CONS_BAUD_RATE )
 # define CONS_BAUD_RATE 115200
 #endif // CONS_BAUD_RATE
 
 
 // Somehow older IDE can't find Wire.h otherwise...
-//# if ARDUINO < 10800 && I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-//# include <Wire.h>
-//# endif // ARDUINO < 10800 && I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+//#if ARDUINO < 10800
+//#include <Wire.h>
+//#endif // ARDUINO < 10800
 
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-////#define I2CDEV_IMPLEMENTATION I2CDEV_ARDUINO_WIRE
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include <Wire.h>
-#endif // I2CDEV_ARDUINO_WIRE
-
-// class default I2C address for the MPU6050 and MPU9150 is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
-
 #ifdef SENSOR_MPU6050
 # include "MPU6050_6Axis_MotionApps20.h"
-MPU6050 mpu( 0x68 + MPU_AD0);
+MPU6050 sensor( 0x68 + MPU_AD0);
 #elif defined( SENSOR_MPU9150 )
 # include "MPU9150_9Axis_MotionApps41.h"
-MPU9150 mpu( 0x68 + MPU_AD0);
+MPU9150 sensor( 0x68 + MPU_AD0);
 #else
 # error No sensor selected!
 #endif
 
-uint16_t   packetSize;        // expected DMP packet size (default is 42 bytes)
+uint16_t   packetSize;                    // expected DMP packet size (default is 42 bytes)
 
-volatile bool mpuInterrupt = false;     // has MPU interrupt pin gone high?
+volatile bool sensorInterrupt = false;    // has sensor interrupt pin gone high?
 
 /*
  *  Handle interrupts from the motion sensor
  */
 void dmpDataReady()
 {
-    mpuInterrupt = true;
+    sensorInterrupt= true;
 }
 
 
@@ -191,15 +203,14 @@ void dmpDataReady()
 void setup_serial()
 {
   
-#ifdef DEBUG
-  Serial.begin( CONS_BAUD_RATE);    // for when DEBUG is defined in MPU6050_6Axis_MotionApps20.h
-#endif // DEBUG
+# ifdef DEBUG
+  Serial.begin( CONS_BAUD_RATE);    // just in case DEBUG is defined in MPU6050_6Axis_MotionApps20.h
+# endif // DEBUG
 
   SerialData.begin( DATA_BAUD_RATE);
+# ifdef CONS_BAUD_RATE
   SerialCons.begin( CONS_BAUD_RATE);
-# ifdef ARDUINO_SAM_DUE
-//  while( ! SerialData )   { ; }   // Needed for native USB port only
-# endif // ARDUINO_SAM_DUE
+# endif // CONS_BAUD_RATE
 
 }
 
@@ -216,10 +227,6 @@ void setup_wire()
 # ifdef ESP32
   Wire.begin( SDA_PIN, SCL_PIN, WIRE_CLOCK*1000UL);
 # else // ESP32
-#   if defined( ARDUINO_BLUEPILL_F103C6 ) || defined( ARDUINO_BLUEPILL_F103C8 ) || defined ( ARDUINO_BLACKPILL_F103C8 ) || defined ( ARDUINO_BLACKPILL_F303CC )
-  Wire.setSDA( SDA_PIN);
-  Wire.setSCL( SCL_PIN);
-#   endif // ARDUINO_BLUEPILL_F103C6 || ARDUINO_BLUEPILL_F103C8 || ARDUINO_BLACKPILL_F103C8 || ARDUINO_BLACKPILL_F303CC
   Wire.begin();
   Wire.setClock(WIRE_CLOCK*1000UL);
 # endif // ESP32
@@ -233,73 +240,83 @@ void setup_wire()
 /*
  *  Initialize the motion sensor
  */
-bool setup_mpu()
+bool setup_sensor()
 {
   bool     value = false;
-  uint8_t  devStatus;         // return status after each device operation (0 = success, !0 = error)
-  uint8_t  mpuIntStatus;      // holds actual interrupt status byte from MPU
+  uint8_t  devStatus;           // return status after each device operation (0 = success, !0 = error)
 
   
   // initialize device
   SerialCons.println(F("Initializing I2C devices..."));
-  mpu.initialize();
+  sensor.initialize();
 
   // verify connection
   SerialCons.println(F("Testing device connections..."));
-  if( mpu.testConnection() )
-    SerialCons.println( F("MPU connection successful"));
+  if( sensor.testConnection() )
+    SerialCons.println( F("sensor connection successful"));
   else
-    SerialCons.println( F("MPU connection failed"));
-
-  // load and configure the DMP
-  SerialCons.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-#ifdef CALIBRATE
-  mpu.setXAccelOffset(ACCEL_OFFSET_X);
-  mpu.setYAccelOffset(ACCEL_OFFSET_Y);
-  mpu.setZAccelOffset(ACCEL_OFFSET_Z);
-  mpu.setXGyroOffset(GYRO_OFFSET_X);
-  mpu.setYGyroOffset(GYRO_OFFSET_Y);
-  mpu.setZGyroOffset(GYRO_OFFSET_Z);
-#endif // CALIBRATE
-
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0) 
   {
-    // turn on the DMP, now that it's ready
-    SerialCons.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // configure interrupt pin as input
-    pinMode( INT_PIN, INPUT);
-    // activate pull-up on interrupt pin
-    digitalWrite( INT_PIN, HIGH);
-    // enable Arduino interrupt detection
-    SerialCons.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-    attachInterrupt(digitalPinToInterrupt(INT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    SerialCons.println(F("DMP ready! Waiting for first interrupt..."));
-    value= true;
+    SerialCons.print( F("sensor connection failed, DeviceID="));
+    SerialCons.print( sensor.getDeviceID(), HEX);
+    SerialCons.println();
   }
-  else 
-  {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    SerialCons.print(F("DMP Initialization failed (code "));
-    SerialCons.print(devStatus);
-    SerialCons.println(F(")"));
 
-    // wait a second before retrying
-    delay( 900);
-  }
+    // load and configure the DMP
+    SerialCons.println(F("Initializing DMP..."));
+    devStatus= sensor.dmpInitialize();
+
+    // make sure it worked (returns 0 if so)
+    if( devStatus == 0 )
+    {
+
+      // turn on the DMP, now that it's ready
+      SerialCons.println(F("Enabling DMP..."));
+      sensor.setDMPEnabled(true);
+
+      // configure interrupt pin as input
+      pinMode( INT_PIN, INPUT);
+      // activate pull-up on interrupt pin?
+//      digitalWrite( INT_PIN, HIGH);
+      // enable Arduino interrupt detection
+      SerialCons.print(F("Enabling interrupt detection (Arduino external interrupt "));
+      SerialCons.print(digitalPinToInterrupt(INT_PIN));
+      SerialCons.println(F(")..."));
+      attachInterrupt(digitalPinToInterrupt(INT_PIN), dmpDataReady, RISING);
+
+      // get expected DMP packet size for later comparison
+      packetSize= sensor.dmpGetFIFOPacketSize();
+
+#     ifdef CALIBRATE
+      sensor.setXAccelOffset(ACCEL_OFFSET_X);
+      sensor.setYAccelOffset(ACCEL_OFFSET_Y);
+      sensor.setZAccelOffset(ACCEL_OFFSET_Z);
+      sensor.setXGyroOffset(GYRO_OFFSET_X);
+      sensor.setYGyroOffset(GYRO_OFFSET_Y);
+      sensor.setZGyroOffset(GYRO_OFFSET_Z);
+#     endif // CALIBRATE
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      SerialCons.println(F("DMP ready! Waiting for first interrupt..."));
+      value= true;
+    }
+    else
+    {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      SerialCons.print(F("DMP Initialization failed (code "));
+      SerialCons.print(devStatus);
+      SerialCons.println(F(")"));
+
+#     ifdef LED_PIN
+      digitalWrite( LED_PIN, LED_OFF);
+#     endif // LED_PIN
+
+      // wait a bit before retrying
+      delay( 800);
+    }
+
   
   return value;
 }
@@ -314,6 +331,10 @@ void setup()
   bool       success;
 
   
+# ifdef LED_PIN
+  pinMode( LED_PIN, OUTPUT);
+# endif // LED_PIN
+
   setup_serial();
 
   SerialCons.print( "Compiled with Arduino IDE version: ");
@@ -322,20 +343,26 @@ void setup()
   success= false;
   while( ! success )
   {
-#ifdef VCC_PIN    // MPU VCC pin hooked to IO pin?  powercycle it!
+#   ifdef LED_PIN
+    digitalWrite( LED_PIN, LED_ON);
+#   endif // LED_PIN
+#   ifdef VCC_PIN    // sensor VCC pin hooked to IO pin?  powercycle it!
     pinMode( VCC_PIN, OUTPUT);
     digitalWrite( VCC_PIN, LOW);
     delay( 100);
     digitalWrite( VCC_PIN, HIGH);
-#endif // VCC_PIN
+    delay( 100);
+#   endif // VCC_PIN
 
     setup_wire();
-    success=setup_mpu();
+    success=setup_sensor();
   }
 
-#ifdef ESP32
-  SerialCons.println( "The device started, now you can pair it with bluetooth!");
-#endif // ESP32
+  SerialCons.println( "The device has been started!");
+
+#   ifdef LED_PIN
+    digitalWrite( LED_PIN, LED_OFF);
+#   endif // LED_PIN
 
 }
 
@@ -368,72 +395,81 @@ void output_relativty(Quaternion q)
  */
 void loop()
 {
-  uint8_t            mpuIntStatus;      // holds actual interrupt status byte from MPU
+  uint8_t            sensorIntStatus;   // holds actual interrupt status byte from sensor
   static uint16_t    fifoCount;         // count of all bytes currently in FIFO
   static uint8_t     fifoBuffer[64];    // FIFO storage buffer
   
   static uint8_t     counter = 0;
-  Quaternion         q;           // [w, x, y, z]         quaternion container
-  VectorInt16        aa;          // [x, y, z]            accel sensor measurements
+  Quaternion         q;                 // [w, x, y, z]         quaternion container
+  VectorInt16        aa;                // [x, y, z]            accel sensor measurements
 
 
-  // wait for MPU interrupt or extra packet(s) available
-  if (!mpuInterrupt && fifoCount < packetSize)
+  // wait for sensor interrupt or extra packet(s) available
+  if( ! sensorInterrupt && fifoCount < packetSize )
     return;
 
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-
   // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
+  fifoCount= sensor.getFIFOCount();
+
+  if( fifoCount < packetSize )
+    return;
+
+  // reset interrupt flag 
+  sensorInterrupt= false;
+
+  // get INT_STATUS byte
+  sensorIntStatus= sensor.getIntStatus();
 
   // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024)
+
+  if( (sensorIntStatus & 0x10) || fifoCount == 1024 )
   {
+#   ifdef LED_PIN
+    digitalWrite( LED_PIN, LED_ON);
+#   endif // LED_PIN
+
     // reset so we can continue cleanly
-    mpu.resetFIFO();
-    SerialCons.println(F("FIFO overflow!"));
+    sensor.resetFIFO();
+    SerialCons.println( F("FIFO overflow!"));
 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
   }
-  else if (mpuIntStatus & 0x02)
+  else if( sensorIntStatus & 0x02 )
   {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize)
-      fifoCount = mpu.getFIFOCount();
+#   ifdef LED_PIN
+    digitalWrite( LED_PIN, LED_OFF);
+#   endif // LED_PIN
 
     // process all packets currently in the FIFO
     while( fifoCount >= packetSize )
     {
   
       // read a packet from FIFO
-      mpu.getFIFOBytes(fifoBuffer, packetSize);
+      sensor.getFIFOBytes( fifoBuffer, packetSize);
   
       // track FIFO count here in case there is > 1 packet available
       // (this lets us immediately read more without waiting for an interrupt)
-      fifoCount -= packetSize;
+      fifoCount-= packetSize;
 
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-
-
-      // report result
-      output_relativty( q);
-
-
-      // about every second?
-      counter %= 100;
-      if( ! counter++ )
-      {
-        
-#if defined( OUTPUT_READABLE_REALACCEL ) || defined( OUTPUT_READABLE_WORLDACCEL )
-        mpu.dmpGetAccel(&aa, fifoBuffer);
-#endif // OUTPUT_READABLE_REALACCEL || OUTPUT_READABLE_WORLDACCEL
-
-        // display values in readable form
-        output_readable( q, aa);
-      }
     }
+
+    sensor.dmpGetQuaternion( &q, fifoBuffer);
+
+
+    // report result
+    output_relativty( q);
+
+
+    // about every second?
+    counter %= 100;
+    if( ! counter++ )
+    {
+      sensor.dmpGetAccel( &aa, fifoBuffer);
+
+      // display values in readable form
+      output_readable( q, aa);
+    }
+
   }
 
 }
@@ -465,7 +501,7 @@ void output_euler(Quaternion q)
   float              euler[3];    // [psi, theta, phi]    Euler angle container
 
       
-  mpu.dmpGetEuler(euler, &q);
+  sensor.dmpGetEuler(euler, &q);
 
   // display Euler angles in degrees
   SerialCons.print("euler(psi,theta,phi):\t\t");
@@ -484,8 +520,8 @@ void output_yawPitchRoll(Quaternion q)
   VectorFloat        gravity;     // [x, y, z]            gravity vector
   float              ypr[3];      // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-  mpu.dmpGetGravity(&gravity, &q);
-  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  sensor.dmpGetGravity(&gravity, &q);
+  sensor.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
   // display Euler angles in degrees
   SerialCons.print("angles(yaw,pitch,roll):\t\t");
@@ -505,8 +541,8 @@ void output_realAccel(Quaternion q,VectorInt16 aa)
   VectorInt16        aaReal;      // [x, y, z]            gravity-free accel sensor measurements
 
 
-  mpu.dmpGetGravity(&gravity, &q);
-  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  sensor.dmpGetGravity(&gravity, &q);
+  sensor.dmpGetLinearAccel(&aaReal, &aa, &gravity);
 
   // display real acceleration, adjusted to remove gravity
   SerialCons.print("realAccel(x,y,z):\t\t");
@@ -526,9 +562,9 @@ void output_worldAccel(Quaternion q,VectorInt16 aa)
   VectorInt16        aaWorld;     // [x, y, z]            world-frame accel sensor measurements
 
 
-  mpu.dmpGetGravity(&gravity, &q);
-  mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-  mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+  sensor.dmpGetGravity(&gravity, &q);
+  sensor.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+  sensor.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
 
   // display initial world-frame acceleration, adjusted to remove gravity
   // and rotated based on known orientation from quaternion
@@ -546,22 +582,22 @@ void output_worldAccel(Quaternion q,VectorInt16 aa)
 void output_readable(Quaternion q,VectorInt16 aa)
 {
 
-#ifdef OUTPUT_READABLE_QUATERNION
+# ifdef OUTPUT_READABLE_QUATERNION
   output_quarernion( q);
-#endif // OUTPUT_READABLE_QUATERNION
-#ifdef OUTPUT_READABLE_EULER
+# endif // OUTPUT_READABLE_QUATERNION
+# ifdef OUTPUT_READABLE_EULER
   output_euler( q);
-#endif // OUTPUT_READABLE_EULER
-#ifdef OUTPUT_READABLE_YAWPITCHROLL
+# endif // OUTPUT_READABLE_EULER
+# ifdef OUTPUT_READABLE_YAWPITCHROLL
   output_yawPitchRoll( q);
-#endif
-#ifdef OUTPUT_READABLE_REALACCEL
+# endif
+# ifdef OUTPUT_READABLE_REALACCEL
   output_realAccel( q, aa);
-#endif // OUTPUT_READABLE_REALACCEL
-#ifdef OUTPUT_READABLE_WORLDACCEL
+# endif // OUTPUT_READABLE_REALACCEL
+# ifdef OUTPUT_READABLE_WORLDACCEL
   output_worldAccel( q, aa);
-#endif // OUTPUT_READABLE_WORLDACCEL
-#if defined( OUTPUT_READABLE_QUATERNION ) \
+# endif // OUTPUT_READABLE_WORLDACCEL
+# if defined( OUTPUT_READABLE_QUATERNION ) \
   || defined( OUTPUT_READABLE_EULER ) \
   || defined( OUTPUT_READABLE_YAWPITCHROLL ) \
   || defined( OUTPUT_READABLE_REALACCEL ) \
